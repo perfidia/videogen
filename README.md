6e1fceba3eddf280c474eb17b3ce24a02313d3cc
Bartosz Alchimowicz <bartosz.alchimowicz@cs.put.poznan.pl>
2013-02-28 18:51:08 +0100

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-videogen
+VideoGen
 ========
 
-from images to video
+Description
+-----------
+
+TODO
+
+Installation
+------------
+
+### Simple
+
+    python setup.py install
+
+### Using eggs
+
+    python setup.py bdist_egg
+    cd dist
+    easy_install <package_name>
+
+Getting started
+---------------
+
+TBD
+
+Authors
+-------
+
+See AUTHORS file.
+
+License
+-------
+
+VideoGen is released under The MIT License. See LICENSE file.
---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup, find_packages
+
+PACKAGE_DIR = 'src'
+
+def read(file_name):
+	return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+
+setup(
+	name = "videogen",
+	version = '0.0.1',
+	author = read("AUTHORS"),
+	keywords = "use cases",
+	url = "https://github.com/perfidia/videogen",
+	package_dir = {'': PACKAGE_DIR},
+	long_description = read("README.md"),
+	packages = find_packages(PACKAGE_DIR, exclude=['ez_setup', 'examples', 'tests']),
+	zip_safe = False,
+)
