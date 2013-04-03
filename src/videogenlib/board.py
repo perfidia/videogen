6a946ba071a945e8806494806070f1de7419717f
Jakub <Jakub@192.168.0.105>
2013-04-03 17:29:32 +0200

    board updated

--- src/videogenlib/board.py
+++ src/videogenlib/board.py
@@ -24,3 +24,14 @@ class Board(object):
         else:
             self._image.paste(im, pos)
 
+    def add_line(self, xy1, xy2, fill, width):
+        self._ctx.line([xy1, xy2], fill, width)
+    
+    def add_square(self, xy1, xy2, fill):
+        self._ctx.rectangle([xy1, xy2], fill)
+        
+    def add_ellipse(self, xy, fill):
+        self._ctx.ellipse(xy, fill)
+        
+        
+        
